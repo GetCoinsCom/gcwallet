6565eee25be9990c25f5e535853fb544237ef52b
Mim <mimi@evergreenatm.io>
2018-07-19 13:41:14 -0400

    Changed bitpay url to gc for terms and policy

--- src/pages/onboarding/disclaimer/disclaimer.ts
+++ src/pages/onboarding/disclaimer/disclaimer.ts
@@ -46,7 +46,7 @@ export class DisclaimerPage {
   }
 
   openDisclaimer() {
-    let url = 'https://bitpay.com/about/terms#wallet';
+    let url = 'https://getcoins.com/about/terms';
     let optIn = true;
     let title = null;
     let message = this.translate.instant('View Wallet Terms of Use');
@@ -63,7 +63,7 @@ export class DisclaimerPage {
   }
 
   openPrivacyPolicy() {
-    let url = 'https://bitpay.com/about/privacy';
+    let url = 'https://getcoins.com/about/privacy';
     let optIn = true;
     let title = null;
     let message = this.translate.instant('View Privacy Policy');
