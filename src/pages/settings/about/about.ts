ce383618272ea657a62e5900aa00993099beb546
Mim <mimi@evergreenatm.io>
2018-07-27 02:42:44 -0400

    Added the original repo button/link

--- src/pages/settings/about/about.ts
+++ src/pages/settings/about/about.ts
@@ -20,6 +20,7 @@ export class AboutPage {
   public version: string;
   public commitHash: string;
   public title: string;
+  public commitHashOrigin: string;
 
   constructor(
     private navCtrl: NavController,
@@ -34,6 +35,7 @@ export class AboutPage {
     this.logger.debug('ionViewDidLoad AboutPage');
     this.commitHash = this.appProvider.info.commitHash;
     this.version = this.appProvider.info.version;
+    this.commitHashOrigin = this.appProvider.info.commitHashOrigin;
     this.title = this.replaceParametersProvider.replace(
       this.translate.instant('About {{appName}}'),
       { appName: this.appProvider.info.nameCase }
@@ -63,7 +65,30 @@ export class AboutPage {
       cancelText
     );
   }
-  /* //this is for when TermsOfUse page will be opened as external link. Disabled becuase we created a page for it instead as below the next func
+  public openCopayLink(): void {
+    let url =
+      'https://github.com/bitpay/' +
+      this.appProvider.info.gitHubRepoNameOrigin +
+      '/tree/' +
+      this.appProvider.info.commitHashOrigin +
+      '';
+    let optIn = true;
+    let title = this.translate.instant('Open GitHub Project');
+    let message = this.translate.instant(
+      'You can see the latest developments and contribute to this open source app by visiting our project on GitHub.'
+    );
+    let okText = this.translate.instant('Open GitHub');
+    let cancelText = this.translate.instant('Go Back');
+    this.externalLinkProvider.open(
+      url,
+      optIn,
+      title,
+      message,
+      okText,
+      cancelText
+    );
+  }
+  /*
     public openTermsOfUse() {
       let url = 'https://getcoins.com/about/';
       let optIn = true;
@@ -84,9 +109,8 @@ export class AboutPage {
   public openTermsOfUse(): void {
     this.navCtrl.push(TermsOfUsePage);
   }
-  /* //disabled due to no reason for such for this app
   public openPrivacyPolicy() {
-    let url = 'https://getcoins.com/about/privacy';
+    let url = 'https://bitpay.com/about/privacy';
     let optIn = true;
     let title = null;
     let message = this.translate.instant('View Privacy Policy');
@@ -101,7 +125,7 @@ export class AboutPage {
       cancelText
     );
   }
-*/
+
   public openSessionLog(): void {
     this.navCtrl.push(SessionLogPage);
   }
