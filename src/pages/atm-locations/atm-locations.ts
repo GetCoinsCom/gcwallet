afe2666671bcc53868c10752ddc62ccfd9559d77
Mim <mimi@evergreenatm.io>
2019-01-16 18:08:47 -0500

    added google map, buttons for direction and call, and loc address

--- src/pages/atm-locations/atm-locations.ts
+++ src/pages/atm-locations/atm-locations.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TranslateService } from '@ngx-translate/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 // import { internals } from 'rx';
 
 import { ExternalLinkProvider } from '../../providers/external-link/external-link';
-import { PopupProvider } from '../../providers/popup/popup';
+// import { PopupProvider } from '../../providers/popup/popup';
 /**
  * Generated class for the AtmLocationsPage page.
  *
@@ -28,13 +29,20 @@ export class AtmLocationsPage {
   public gcATMName: string = 'GetCoins Bitcoin ATM';
   public googleDirectionUrl: string;
   public encodedDirection: string;
+  public googleDirUrl: string;
+  public myLocation: object = { lat: 41.234648, lng: -82.254409 };
+  public encodedMyLocation: string;
+  public travelMode: string = 'driving';
+  public address: string;
+  public hours: string;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private translate: TranslateService,
     private externalLinkProvider: ExternalLinkProvider,
-    private popupProvider: PopupProvider
+    // private popupProvider: PopupProvider,
+    private iab: InAppBrowser
   ) {
     this.id = navParams.get('locationId');
     this.data = navParams.get('dataSet');
@@ -50,6 +58,9 @@ export class AtmLocationsPage {
         this.data.state +
         ' ' +
         this.data.zipcode
+    );
+    this.encodedMyLocation = encodeURIComponent(
+      this.myLocation['lat'] + ',' + this.myLocation['lng']
     );
     this.encodedDirection = encodeURIComponent(
       this.gcATMName +
@@ -68,6 +79,14 @@ export class AtmLocationsPage {
       '&q=' +
       this.encodedAddress +
       '&zoom=14';
+
+    this.googleDirUrl =
+      'https://www.google.com/maps/dir/?api=1&origin=' +
+      this.encodedMyLocation +
+      '&destination=' +
+      this.encodedDirection +
+      '&travelmode=' +
+      this.travelMode;
   }
 
   ionViewDidLoad() {
@@ -75,6 +94,7 @@ export class AtmLocationsPage {
     console.log(this.id);
     console.log(this.data);
     console.log(this.googleUrl);
+    console.log(this.googleDirUrl);
     console.log(this.serverJson);
     console.log(this.localJson);
   }
@@ -96,14 +116,32 @@ export class AtmLocationsPage {
   //       }
   //     });
   // }
-  public openDirectionPage(): void {
-    let url = 'https://maps.googleapis.com/maps/api/directions/json?origin=';
+  public getDirectionPage(): void {
+    const browser = this.iab.create(this.googleDirUrl, '_blank');
+    browser.show();
+  }
+  public openDirectionLink(): void {
+    let url = this.googleDirUrl;
     let optIn = true;
     let title = null;
-    let message = this.translate.instant(
-      'Go visit our site at www.getcoins.com'
-    );
+    let message = this.translate.instant('Open Google Map');
     let okText = this.translate.instant('Direction');
+    let cancelText = this.translate.instant('Go Back');
+    this.externalLinkProvider.open(
+      url,
+      optIn,
+      title,
+      message,
+      okText,
+      cancelText
+    );
+  }
+  public callCustomerSupport(): void {
+    let url = 'tel:+1-860-800-2646';
+    let optIn = true;
+    let title = null;
+    let message = this.translate.instant('You can call us now at 860-800-2646');
+    let okText = this.translate.instant('Call');
     let cancelText = this.translate.instant('Go Back');
     this.externalLinkProvider.open(
       url,
