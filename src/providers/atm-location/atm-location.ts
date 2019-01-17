62b5a9c8664efa27ae826d2d3f322aff5691a119
Mim <mimi@evergreenatm.io>
2019-01-17 07:21:19 -0500

    created the api call from v1 api and added comments

--- src/providers/atm-location/atm-location.ts
+++ src/providers/atm-location/atm-location.ts
@@ -12,23 +12,33 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AtmLocationProvider {
   private apiTerm: string = 'locations';
-  private url;
+  // private url;
   private locurl;
+  private locurl_old;
+
   constructor(public http: HttpClient) {
     console.log('Hello AtmLocationProvider Provider');
-    this.url = 'https://jsonplaceholder.typicode.com/photos';
-    this.locurl = 'https://getcoins.com/api/locations/index.php';
+    // this.url = 'https://jsonplaceholder.typicode.com/photos';
+    this.locurl_old = 'https://getcoins.com/api/locations/index.php';
+    this.locurl = 'https://getcoins.com/api/v1/location/read.php';
   }
 
-  public getLocations() {
-    return this.http.get(this.locurl + '?q=' + this.apiTerm);
+  //** To get the whole locations by the premature API */
+  public getLocationsOld() {
+    return this.http.get(this.locurl_old + '?q=' + this.apiTerm);
     // .map((res: Response) => res.json());
     // return this.http.get(this.url + '?albumId=' + albumId);
   }
-  public getPosts(albumId) {
-    // return this.http.get(this.locurl)
-    //   .map((res: Response) => res.json());
-    return this.http.get(this.url + '?albumId=' + albumId);
+  //** To get the whole locations by the final API */
+  public getLocations() {
+    return this.http.get(this.locurl);
     // .map((res: Response) => res.json());
+    // return this.http.get(this.url + '?albumId=' + albumId);
   }
+  // public getPosts(albumId) {
+  //   // return this.http.get(this.locurl)
+  //   //   .map((res: Response) => res.json());
+  //   return this.http.get(this.url + '?albumId=' + albumId);
+  //   // .map((res: Response) => res.json());
+  // }
 }
