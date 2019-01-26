0d4386b4693e1fffd6921f4ca8d084d0afbd30e3
Mim <mimi@evergreenatm.io>
2019-01-25 21:40:08 -0500

    Added comments for each func; clean now

--- src/pipes/store-name.ts
+++ src/pipes/store-name.ts
@@ -10,23 +10,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StoreNamePipe implements PipeTransform {
   /**
-   * Takes a value and makes it lowercase.
+   * Takes the "name" value passed in the template and convert it to desired format
+   * @param value: string
    */
   transform(value: string) {
     return this.splitAndGrab(value);
   }
+  /**
+   * Split the name data and grab only the name of the store except for 76 Gas Station
+   * @param value
+   * @returns newValue[0] or value
+   */
   splitAndGrab(value: string) {
     let newValue: string[] = new Array();
     if (value.indexOf('Gas') >= 0) {
       console.log('the name included Gas');
       if (value === '76 Gas Station') {
         newValue = value.split(' Station');
-        // console.log(newValue, ' from the first 76 if within Gas');
       } else {
         newValue = value.split(' Gas Station');
-        // console.log(newValue, ' from the else if within Gas');
       }
-      // console.log(newValue[0]);
       return newValue[0];
     }
     return value;
